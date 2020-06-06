290133194f40c47cd857bce6871ddbdc3b7c9e7c
Trieu Nguyen <47194907+HaiTrieuNg@users.noreply.github.com>
2020-06-06 13:09:31 -0700

    Add files via upload

--- source/conf.py
+++ source/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'Test'
-copyright = '2020, Trieu Nguyen'
-author = 'Trieu Nguyen'
+copyright = '2020, BeeFi.io'
+author = 'BeeFi'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
@@ -48,7 +48,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
+html_logo = 'BeeFi_Logo.png'
 master_doc = 'index'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
