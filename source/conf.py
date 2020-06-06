e23595308cc3c873c016e9ba37ff5f74cb015f35
Trieu Nguyen <47194907+HaiTrieuNg@users.noreply.github.com>
2020-06-06 14:43:06 -0700

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -47,6 +47,11 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_context = {
+"display_github": False, # Add 'Edit on Github' link instead of 'View page source'
+"last_updated": True,
+"commit": False,
+}
 html_theme = 'sphinx_rtd_theme'
 html_logo = 'BeeFi_Logo.png'
 master_doc = 'index'
